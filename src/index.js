fedce5edd8fb5f11a8e4a9fc4390608c78045118
thomasvergne <thomas.vergne.contact@gmail.com>
2023-03-29 12:35:23 +0200

    ⚡️ Added loading message.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ const plugin = require('tailwindcss/plugin');
 const base = require('../dist/output');
 
 module.exports = plugin(function({ addUtilities, addBase, addComponents }) {
+  console.log('[INIT] Le don vert')
   addUtilities(base);
+  console.log('[INIT] Le don vert (done)');
 });
